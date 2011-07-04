7619bce80345128c6f5531a9b0f6cb316f1e7fa6
SoftwareBotanyDan_cp <SND\SoftwareBotanyDan_cp>
2011-07-04 04:41:13 +0000

    Split out Vector unsafe logic into a separate file.  Prepped the safe section.  ALL 4 Safe build configurations clearly broken.

--- Main/SoftwareBotany.Sunlight/Vector.cs
+++ Main/SoftwareBotany.Sunlight/Vector.cs
@@ -85,50 +85,6 @@ namespace SoftwareBotany.Sunlight
             }
         }
 
-        private unsafe void Decompress(Vector vector)
-        {
-            fixed (Word* iFixed = _words, jFixed = vector._words)
-            {
-                Word* i = iFixed;
-                Word* j = jFixed;
-                Word* jMax = jFixed + vector._wordCountPhysical;
-
-                while (j < jMax)
-                {
-                    if (j->IsCompressed)
-                    {
-                        if (j->FillBit)
-                        {
-                            Word* k = i + j->FillCount;
-
-                            while (i < k)
-                            {
-                                i->Raw = 0x7FFFFFFF;
-                                i++;
-                            }
-                        }
-                        else
-                            i += j->FillCount;
-
-#if POSITIONLIST
-                        if (j->HasPackedWord)
-                        {
-                            i->Raw = j->PackedWord.Raw;
-                            i++;
-                        }
-#endif
-                    }
-                    else
-                    {
-                        i->Raw = j->Raw;
-                        i++;
-                    }
-
-                    j++;
-                }
-            }
-        }
-
         #endregion
 
         #region Rebuild
@@ -644,7 +600,7 @@ namespace SoftwareBotany.Sunlight
 
         #endregion
 
-        #region And
+        #region Logical Operations
 
         public void And(Vector vector)
         {
@@ -686,64 +642,6 @@ namespace SoftwareBotany.Sunlight
 #endif
         }
 
-        private unsafe static Word* AndCompressed(Word* i, Word* iMax, Word* j, Word* jMax)
-        {
-            while (i < iMax && j < jMax)
-            {
-                if (j->IsCompressed)
-                {
-                    if (!j->FillBit)
-                    {
-                        Word* k = i + j->FillCount;
-
-                        if (k >= iMax)
-                            break;
-
-                        while (i < k)
-                        {
-                            i->Raw = 0;
-                            i++;
-                        }
-                    }
-                    else
-                        i += j->FillCount;
-
-#if POSITIONLIST
-                    if (j->HasPackedWord && i < iMax)
-                    {
-                        i->Raw &= j->PackedWord.Raw;
-                        i++;
-                    }
-#endif
-                }
-                else
-                {
-                    i->Raw &= j->Raw;
-                    i++;
-                }
-
-                j++;
-            }
-
-            return i;
-        }
-
-        private unsafe static Word* AndUncompressed(Word* i, Word* iMax, Word* j, Word* jMax)
-        {
-            while (i < iMax && j < jMax)
-            {
-                i->Raw &= j->Raw;
-                i++;
-                j++;
-            }
-
-            return i;
-        }
-
-        #endregion
-
-        #region AndPopulation
-
         public int AndPopulation(Vector vector)
         {
             if (vector == null)
@@ -771,78 +669,6 @@ namespace SoftwareBotany.Sunlight
 #else
 #endif
         }
-
-        private unsafe static int AndPopulationCompressed(Word* i, Word* iMax, Word* j, Word* jMax)
-        {
-            int population = 0;
-
-            while (i < iMax && j < jMax)
-            {
-                if (j->IsCompressed)
-                {
-                    if (j->FillBit)
-                    {
-                        Word* k = i + j->FillCount;
-
-                        if (k > iMax)
-                            k = iMax;
-
-                        while (i < k)
-                        {
-                            population += i->Population;
-                            i++;
-                        }
-                    }
-                    else
-                        i += j->FillCount;
-
-#if POSITIONLIST
-                    if (j->HasPackedWord && i < iMax)
-                    {
-                        if (i->Raw > 0 && (i->Raw & j->PackedWord.Raw) > 0)
-                            population++;
-
-                        i++;
-                    }
-#endif
-                }
-                else
-                {
-                    uint word = i->Raw & j->Raw;
-
-                    if (word > 0)
-                        population += word.Population();
-
-                    i++;
-                }
-
-                j++;
-            }
-
-            return population;
-        }
-
-        private unsafe static int AndPopulationUncompressed(Word* i, Word* iMax, Word* j, Word* jMax)
-        {
-            int population = 0;
-
-            while (i < iMax && j < jMax)
-            {
-                uint word = i->Raw & j->Raw;
-
-                if (word > 0)
-                    population += word.Population();
-
-                i++;
-                j++;
-            }
-
-            return population;
-        }
-
-        #endregion
-
-        #region Or
 
         public void Or(Vector vector)
         {
@@ -877,55 +703,8 @@ namespace SoftwareBotany.Sunlight
             }
 #else
 #endif
-        }
-
-        private unsafe static void OrCompressed(Word* i, Word* iMax, Word* j, Word* jMax)
-        {
-            while (i < iMax && j < jMax)
-            {
-                if (j->IsCompressed)
-                {
-                    if (j->FillBit)
-                    {
-                        Word* k = i + j->FillCount;
-
-                        while (i < k)
-                        {
-                            i->Raw = 0x7FFFFFFF;
-                            i++;
-                        }
-                    }
-                    else
-                        i += j->FillCount;
-
-#if POSITIONLIST
-                    if (j->HasPackedWord && i < iMax)
-                    {
-                        i->Raw |= j->PackedWord.Raw;
-                        i++;
-                    }
-#endif
-                }
-                else
-                {
-                    i->Raw |= j->Raw;
-                    i++;
-                }
-
-                j++;
-            }
-        }
-
-        private unsafe static void OrUncompressed(Word* i, Word* iMax, Word* j, Word* jMax)
-        {
-            while (i < iMax && j < jMax)
-            {
-                i->Raw |= j->Raw;
-                i++;
-                j++;
-            }
-        }
 
         #endregion
+        }
     }
 }
---
+++ Main/SoftwareBotany.Sunlight/VectorLogicSafe.cs
@@ -0,0 +1,241 @@
+﻿#if !UNSAFE
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace SoftwareBotany.Sunlight
+{
+    public partial class Vector
+    {
+        #region Construction
+
+        private unsafe void Decompress(Vector vector)
+        {
+            fixed (Word* iFixed = _words, jFixed = vector._words)
+            {
+                Word* i = iFixed;
+                Word* j = jFixed;
+                Word* jMax = jFixed + vector._wordCountPhysical;
+
+                while (j < jMax)
+                {
+                    if (j->IsCompressed)
+                    {
+                        if (j->FillBit)
+                        {
+                            Word* k = i + j->FillCount;
+
+                            while (i < k)
+                            {
+                                i->Raw = 0x7FFFFFFF;
+                                i++;
+                            }
+                        }
+                        else
+                            i += j->FillCount;
+
+#if POSITIONLIST
+                        if (j->HasPackedWord)
+                        {
+                            i->Raw = j->PackedWord.Raw;
+                            i++;
+                        }
+#endif
+                    }
+                    else
+                    {
+                        i->Raw = j->Raw;
+                        i++;
+                    }
+
+                    j++;
+                }
+            }
+        }
+
+        #endregion
+
+        #region And
+
+        private unsafe static Word* AndCompressed(Word* i, Word* iMax, Word* j, Word* jMax)
+        {
+            while (i < iMax && j < jMax)
+            {
+                if (j->IsCompressed)
+                {
+                    if (!j->FillBit)
+                    {
+                        Word* k = i + j->FillCount;
+
+                        if (k >= iMax)
+                            break;
+
+                        while (i < k)
+                        {
+                            i->Raw = 0;
+                            i++;
+                        }
+                    }
+                    else
+                        i += j->FillCount;
+
+#if POSITIONLIST
+                    if (j->HasPackedWord && i < iMax)
+                    {
+                        i->Raw &= j->PackedWord.Raw;
+                        i++;
+                    }
+#endif
+                }
+                else
+                {
+                    i->Raw &= j->Raw;
+                    i++;
+                }
+
+                j++;
+            }
+
+            return i;
+        }
+
+        private unsafe static Word* AndUncompressed(Word* i, Word* iMax, Word* j, Word* jMax)
+        {
+            while (i < iMax && j < jMax)
+            {
+                i->Raw &= j->Raw;
+                i++;
+                j++;
+            }
+
+            return i;
+        }
+
+        #endregion
+
+        #region AndPopulation
+
+        private unsafe static int AndPopulationCompressed(Word* i, Word* iMax, Word* j, Word* jMax)
+        {
+            int population = 0;
+
+            while (i < iMax && j < jMax)
+            {
+                if (j->IsCompressed)
+                {
+                    if (j->FillBit)
+                    {
+                        Word* k = i + j->FillCount;
+
+                        if (k > iMax)
+                            k = iMax;
+
+                        while (i < k)
+                        {
+                            population += i->Population;
+                            i++;
+                        }
+                    }
+                    else
+                        i += j->FillCount;
+
+#if POSITIONLIST
+                    if (j->HasPackedWord && i < iMax)
+                    {
+                        if (i->Raw > 0 && (i->Raw & j->PackedWord.Raw) > 0)
+                            population++;
+
+                        i++;
+                    }
+#endif
+                }
+                else
+                {
+                    uint word = i->Raw & j->Raw;
+
+                    if (word > 0)
+                        population += word.Population();
+
+                    i++;
+                }
+
+                j++;
+            }
+
+            return population;
+        }
+
+        private unsafe static int AndPopulationUncompressed(Word* i, Word* iMax, Word* j, Word* jMax)
+        {
+            int population = 0;
+
+            while (i < iMax && j < jMax)
+            {
+                uint word = i->Raw & j->Raw;
+
+                if (word > 0)
+                    population += word.Population();
+
+                i++;
+                j++;
+            }
+
+            return population;
+        }
+
+        #endregion
+
+        #region Or
+
+        private unsafe static void OrCompressed(Word* i, Word* iMax, Word* j, Word* jMax)
+        {
+            while (i < iMax && j < jMax)
+            {
+                if (j->IsCompressed)
+                {
+                    if (j->FillBit)
+                    {
+                        Word* k = i + j->FillCount;
+
+                        while (i < k)
+                        {
+                            i->Raw = 0x7FFFFFFF;
+                            i++;
+                        }
+                    }
+                    else
+                        i += j->FillCount;
+
+#if POSITIONLIST
+                    if (j->HasPackedWord && i < iMax)
+                    {
+                        i->Raw |= j->PackedWord.Raw;
+                        i++;
+                    }
+#endif
+                }
+                else
+                {
+                    i->Raw |= j->Raw;
+                    i++;
+                }
+
+                j++;
+            }
+        }
+
+        private unsafe static void OrUncompressed(Word* i, Word* iMax, Word* j, Word* jMax)
+        {
+            while (i < iMax && j < jMax)
+            {
+                i->Raw |= j->Raw;
+                i++;
+                j++;
+            }
+        }
+
+        #endregion
+    }
+}
+#endif
