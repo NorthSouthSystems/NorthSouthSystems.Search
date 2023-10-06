37fcf143aa5cef112488285c83945810a03b3d39
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2023-10-06 13:00:59 -0400

    Eliminated the VectorLogicSafe T4 template because of future plans to use different assemblies with conditional compilation instead for Compressed vs. CompressedWithPackedPosition logic.

--- FOSStrich.Search/VectorLogicSafe.cs
+++ FOSStrich.Search/VectorLogicSafe.cs
@@ -4,6 +4,86 @@ using System.Numerics;
 
 internal sealed partial class VectorLogicSafe : IVectorLogic
 {
+    #region Decompress
+
+    void IVectorLogic.DecompressInPlaceNoneCompressed(Word[] iWords, Word[] jWordsBuffer, int jWordCountPhysical)
+    {
+        int i = 0;
+        int j = 0;
+        var jWords = new Span<Word>(jWordsBuffer, 0, jWordCountPhysical);
+
+        while (j < jWords.Length)
+        {
+            Word jWord = jWords[j];
+
+            if (jWord.IsCompressed)
+            {
+                if (jWord.FillBit)
+                {
+                    int k = i + jWord.FillCount;
+
+                    while (i < k)
+                    {
+                        iWords[i].Raw = 0x7FFFFFFF;
+                        i++;
+                    }
+                }
+                else
+                    i += jWord.FillCount;
+            }
+            else
+            {
+                iWords[i].Raw = jWord.Raw;
+                i++;
+            }
+
+            j++;
+        }
+    }
+
+    void IVectorLogic.DecompressInPlaceNoneCompressedWithPackedPosition(Word[] iWords, Word[] jWordsBuffer, int jWordCountPhysical)
+    {
+        int i = 0;
+        int j = 0;
+        var jWords = new Span<Word>(jWordsBuffer, 0, jWordCountPhysical);
+
+        while (j < jWords.Length)
+        {
+            Word jWord = jWords[j];
+
+            if (jWord.IsCompressed)
+            {
+                if (jWord.FillBit)
+                {
+                    int k = i + jWord.FillCount;
+
+                    while (i < k)
+                    {
+                        iWords[i].Raw = 0x7FFFFFFF;
+                        i++;
+                    }
+                }
+                else
+                    i += jWord.FillCount;
+
+                if (jWord.HasPackedWord)
+                {
+                    iWords[i].Raw = jWord.PackedWord.Raw;
+                    i++;
+                }
+            }
+            else
+            {
+                iWords[i].Raw = jWord.Raw;
+                i++;
+            }
+
+            j++;
+        }
+    }
+
+    #endregion
+
     #region And In-Place
 
     void IVectorLogic.AndInPlaceNoneNone(Word[] iWordsBuffer, ref int iWordCountPhysical, ref int iWordCountLogical, Word[] jWordsBuffer, int jWordCountPhysical)
