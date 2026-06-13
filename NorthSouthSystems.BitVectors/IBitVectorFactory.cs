00c15598e5c8ac550f696355b1d293e44f8aba69
FreeAndWithBeer <30539567+FreeAndWithBeer@users.noreply.github.com>
2026-06-13 09:05:23 -0400

    Completed merge of NorthSouthSystems.BitVectors.WAH into this repo.

---
+++ NorthSouthSystems.BitVectors/IBitVectorFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace NorthSouthSystems.BitVectors;
+
+public interface IBitVectorFactory<TBitVector>
+    where TBitVector : IBitVector<TBitVector>
+{
+    TBitVector Create(bool isCompressed);
+    TBitVector Create(bool isCompressed, TBitVector copy);
+    TBitVector CreateUncompressedUnion(params TBitVector[] bitVectors);
+
+    int WordSize { get; }
+}
